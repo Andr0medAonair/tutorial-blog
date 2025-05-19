b7797174b32a5ff4f8a2e1b132a0c70e6e4f7794
Rafaela Janeczko <sagethirteen@gmail.com>
2025-05-19 16:40:21 -0300

    feat: separate featured post component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Container } from "@/components/Container";
-import { CoverImage } from "@/components/CoverImage";
 import { Header } from "@/components/Header";
-import { HighlightedPost } from "@/components/HighlightedPost";
+import { PostFeatured } from "@/components/PostFeatured";
 import { PostsList } from "@/components/PostsList";
 import { Spinner } from "@/components/Spinner";
 import clsx from "clsx";
@@ -9,33 +8,14 @@ import { Suspense } from "react";
 
 export default async function HomePage() {
   const classes = clsx("text-6xl", "font-bold", "text-center", "py-8");
-  const columnClasses = clsx(
-    "grid grid-cols-1 gap-8 mb-16",
-    "sm:grid-cols-2 group"
-  );
 
   return (
     <Container>
       <Header />
-      <section className={columnClasses}>
-        <CoverImage
-          linkProps={{
-            href: "/post/asdfasdf",
-          }}
-          imageProps={{
-            width: 1200,
-            height: 720,
-            src: "/images/bryen_9.png",
-            alt: "Alt da imagem",
-            priority: true,
-          }}
-        />
+      <Suspense fallback={<Spinner />}>
+        <PostFeatured />
+      </Suspense>
 
-        <HighlightedPost
-          title="Rerum, vel ex? Impedit ullam harum blanditiis"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit dolorem est dolor porro, doloribus neque, quidem mollitia doloremque, ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam harum blanditiis mollitia?"
-        />
-      </section>
       <Suspense fallback={<Spinner />}>
         <PostsList />
       </Suspense>
---
+++ src/components/PostFeatured/index.tsx
@@ -0,0 +1,29 @@
+import clsx from "clsx";
+import { CoverImage } from "../CoverImage";
+import { HighlightedPost } from "../HighlightedPost";
+
+export function PostFeatured() {
+  const classes = clsx("grid grid-cols-1 gap-8 mb-16", "sm:grid-cols-2 group");
+
+  return (
+    <section className={classes}>
+      <CoverImage
+        linkProps={{
+          href: "/post/asdfasdf",
+        }}
+        imageProps={{
+          width: 1200,
+          height: 720,
+          src: "/images/bryen_9.png",
+          alt: "Alt da imagem",
+          priority: true,
+        }}
+      />
+
+      <HighlightedPost
+        title="Rerum, vel ex? Impedit ullam harum blanditiis"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. In velit dolorem est dolor porro, doloribus neque, quidem mollitia doloremque, ad perspiciatis fugiat. Rerum, vel ex? Impedit ullam harum blanditiis mollitia?"
+      />
+    </section>
+  );
+}
